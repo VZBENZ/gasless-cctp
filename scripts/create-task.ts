69c78e510a3bdb865138b066180262ac87d1a2c1
MutableAI <mutableai@github.com>
2023-10-06 05:20:26 +0000

    [code modification] Editing /gasless-cctp/scripts/create-task.ts

--- scripts/create-task.ts
+++ scripts/create-task.ts
@@ -5,41 +5,43 @@ import {
   TriggerType,
 } from "@gelatonetwork/automate-sdk";
 
-const main = async () => {
-  const cctpW3f = w3f.get("gelato-cctp");
-  const cctpCid = await cctpW3f.deploy();
+async function main() {
+  try {
+    const cctpW3f = w3f.get("gelato-cctp");
+    const cctpCid = await cctpW3f.deploy();
 
-  const [deployer] = await ethers.getSigners();
-  const { chainId } = await ethers.provider.getNetwork();
+    const [deployer] = await ethers.getSigners();
+    const { chainId } = await ethers.provider.getNetwork();
 
-  // required since the automate-sdk uses ethers v5
-  // eslint-disable-next-line
-  (deployer as any)._isSigner = true;
+   // required since the automate-sdk uses ethers v5
+    // eslint-disable-next-line
+    (deployer as any)._isSigner = true;
 
-  // eslint-disable-next-line
-  const automate = new AutomateSDK(Number(chainId), deployer as any);
+    // eslint-disable-next-line
+    const automate = new AutomateSDK(Number(chainId), deployer as any);
 
-  const trigger: TriggerConfig = {
-    type: TriggerType.TIME,
-    interval: 30000,
-  };
+    const trigger: TriggerConfig = {
+      type: TriggerType.TIME,
+      interval: 30000,
+    };
 
-  const { taskId, tx } = await automate.createBatchExecTask({
-    name: "Gelato CCTP",
-    web3FunctionHash: cctpCid,
-    web3FunctionArgs: {},
-    useTreasury: false,
-    trigger,
-  });
+    const { taskId, tx } = await automate.createBatchExecTask({
+      name: "Gelato CCTP",
+      web3FunctionHash: cctpCid,
+      web3FunctionArgs: {},
+      useTreasury: false,
+      trigger,
+    });
 
-  await tx.wait();
+    await tx.wait();
 
-  console.log(
-    `https://beta.app.gelato.network/task/${taskId}?chainId=${chainId}`
-  );
-};
+    console.log(
+      `https://beta.app.gelato.network/task/${taskId}?chainId=${chainId}`
+    );
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main();
